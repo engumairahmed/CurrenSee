337f2cdcc5a18b37e145d13de6a0aa71cf83863f
engumairahmed <eng.umair.a@gmail.com>
2024-05-24 15:42:14 +0500

    Update api_endpoints.dart

--- lib/api_endpoints.dart
+++ lib/api_endpoints.dart
@@ -3,32 +3,32 @@
 // ------> Login URL
 
 // String loginurl="https://currensee.000webhostapp.com/auth/login.php";
-String loginurl = "http://localhost/phpapi/currensee/auth/login.php";
+String loginurl = "http://localhost/currensee/auth/login.php";
 
 // ------> Register URL
 
 // String registerurl="https://currensee.000webhostapp.com/auth/register.php";
-String registerurl = "http://localhost/phpapi/currensee/auth/register.php";
+String registerurl = "http://localhost/currensee/auth/register.php";
 
 // ------> User Password Update URL
 
 // String userPasswordUpdateurl = "https://currensee.000webhostapp.com/auth/changepassword.php";
-String userPasswordUpdateurl = "http://localhost/phpapi/currensee/auth/changepassword.php";
+String userPasswordUpdateurl = "http://localhost/currensee/auth/changepassword.php";
 
 // ------> User Profile URL
 
 // String userDataUrl="https://currensee.000webhostapp.com/auth/user.php?id=";
-String userDataUrl = "http://localhost/phpapi/currensee/auth/user.php?id=";
+String userDataUrl = "http://localhost/currensee/auth/user.php?id=";
 
 // ------> Firbase Profile UID verification URL
 
 // String uidCheckUrl="https://currensee.000webhostapp.com/auth/uid.php?id=";
-String uidCheckUrl = "http://localhost/phpapi/currensee/auth/uid.php?id=";
+String uidCheckUrl = "http://localhost/currensee/auth/uid.php?id=";
 
 // ------> Conversion URL
 
 // String conversionUrl="https://currensee.000webhostapp.com/conversion/convert.php";
-String conversionUrl = "http://localhost/phpapi/currensee/conversion/convert.php";
+String conversionUrl = "http://localhost/currensee/conversion/convert.php";
 
 // ------> Forgot Password URL
 
@@ -37,21 +37,21 @@ String forgotpasswordurl = "https://currensee.000webhostapp.com/auth/reset.php";
 // ------> Feedback URL
 
 // String feedbackurl="https://currensee.000webhostapp.com/feedback.php";
-String feedbackurl = "http://localhost/phpapi/currensee/feedback.php";
+String feedbackurl = "http://localhost/currensee/feedback.php";
 
 // ------> Get Feedback Data URL
 
-String feedbackDataURL = "http://localhost/phpapi/currensee/getfeedback.php?id=";
+String feedbackDataURL = "http://localhost/currensee/getfeedback.php?id=";
 
 // ------> Historical Rates URL
 
 // String historicalRatesURL="https://currensee.000webhostapp.com/conversion/historical.php";
-String historicalRatesURL = "http://localhost/phpapi/currensee/conversion/historical.php";
+String historicalRatesURL = "http://localhost/currensee/conversion/historical.php";
 
 // ------> Get Currency Codes URL
 
 String currencyCodesURL="https://currensee.000webhostapp.com/conversion/getcurrencycodes.php";
-// String currencyCodesURL = "http://localhost/phpapi/currensee/conversion/getcurrencycodes.php";
+// String currencyCodesURL = "http://localhost/currensee/conversion/getcurrencycodes.php";
 
 // ------> Get Currency List URL
 
@@ -60,17 +60,17 @@ String currencyListURL="https://currensee.000webhostapp.com/conversion/getcurren
 // ------> User Conversion History URL
 
 // String conversionHistoryURL = "https://currensee.000webhostapp.com/conversion/conversionhistory.php?id=";
-String conversionHistoryURL = "http://localhost/phpapi/currensee/conversion/conversionhistory.php?id=";
+String conversionHistoryURL = "http://localhost/currensee/conversion/conversionhistory.php?id=";
 
 
 // ------> User Preferences URL
 
-String userPreferencesURL = "http://localhost/phpapi/currensee/userpreferences.php";
+String userPreferencesURL = "http://localhost/currensee/userpreferences.php";
 // String userPreferencesURL = "https://currensee.000webhostapp.com/userpreferences.php";
 
 
 
 // ------> Fetch User Preferences URL
 
-String getPreferencesURL = "http://localhost/phpapi/currensee/getuserpreferences.php?id=";
+String getPreferencesURL = "http://localhost/currensee/getuserpreferences.php?id=";
 // String getPreferencesURL = "https://currensee.000webhostapp.com/getuserpreferences.php?id=";
